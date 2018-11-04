288309b318df5f4c3cb1168e861f2c63853f34ed
dkaslovsky <dkaslovsky@gmail.com>
2018-11-03 22:09:48 -0600

    remove dependency on networkx for connected components

--- graphrole/features/similarity.py
+++ graphrole/features/similarity.py
@@ -1,10 +1,13 @@
 import itertools as it
-from typing import Iterator, Set, Union
+from collections import defaultdict
+from typing import (Any, Dict, Iterable, Iterator, NewType, Optional, Set,
+                    Tuple, Union)
 
-import networkx as nx
 import numpy as np
 import pandas as pd
 from scipy.spatial.distance import pdist
+
+Node = NewType('Node', Union[int, str])
 
 
 def vertical_log_binning(
@@ -59,7 +62,7 @@ def vertical_log_binning(
 def group_features(
     binned_features: Union[np.ndarray, pd.DataFrame],
     dist_thresh: int = 0
-) -> Iterator[Set[int]]:
+) -> Iterator[Set[Node]]:
     """
     Group features according to connected components of graph
     induced by pairwise distances below distance threshold
@@ -81,5 +84,59 @@ def group_features(
 
     all_edges = it.combinations(nodes, 2)
     edges = it.compress(all_edges, dists <= dist_thresh)
-    groups = nx.connected_components(nx.Graph(edges))
+    groups = _get_connected_components_from_edges(edges)
     return groups
+
+
+def _get_connected_components_from_edges(
+    edges: Iterable[Tuple[Node]]
+) -> Iterator[Set[Node]]:
+    """
+    Generate connected components represented as sets of nodes
+    :param edges: iterable of edges represented as (node1, node2) tuples
+    """
+    # get dict mapping node to neighors
+    adj_dict = _get_adj_dict(edges)
+    # maintain set of all nodes already accounted for
+    visited = set()
+    # run dfs over all nodes not previously visited
+    for node in adj_dict.keys():
+        if node not in visited:
+            component = _dfs(adj_dict, node)
+            visited.update(component)
+            yield component
+
+
+def _get_adj_dict(
+    edges: Iterable[Tuple[Node]]
+) -> Dict[Node, Set[Node]]:
+    """
+    Construct dict mapping node to a set of its neighbor nodes
+    :param edges: iterable of edges represented as (node1, node2) tuples
+    """
+    adj = defaultdict(set)
+    for (node1, node2) in edges:
+        adj[node1].add(node2)
+        adj[node2].add(node1)
+    return dict(adj)
+
+
+def _dfs(
+    adj_dict: Dict[Any, Set],
+    node: Node,
+    visited: Optional[Set] = None
+) -> Set:
+    """
+    Run recursive depth first search starting from node and
+    return set of all visited nodes
+    :param adj_dict: dict mapping node to set of neighbor nodes
+    :param node: node at which to start search
+    :param visited: set of all nodes visited, initially should be None
+    """
+    if not visited:
+        visited = set()
+    visited.add(node)
+    next_level_unvisited = adj_dict[node] - visited
+    for nbr in next_level_unvisited:
+        _dfs(adj_dict, nbr, visited)
+    return visited
