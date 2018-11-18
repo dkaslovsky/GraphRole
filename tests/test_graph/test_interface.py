981c8f671d75f9318d71ad1ebcc684199d0f07c7
dkaslovsky <dkaslovsky@gmail.com>
2018-11-18 15:54:25 -0700

    update tests for new module structure

--- tests/test_graph/test_interface.py
+++ tests/test_graph/test_interface.py
@@ -1,9 +1,12 @@
+import itertools as it
 import unittest
 
+import igraph as ig
 import networkx as nx
+import numpy as np
+import pandas as pd
 
-from graphrole.graph.graph import Graph
-from graphrole.graph.interface import get_interface
+from graphrole.graph import interface
 
 
 class TestGetInterface(unittest.TestCase):
@@ -13,14 +16,113 @@ class TestGetInterface(unittest.TestCase):
     def test_get_interface(self):
         # test with object from supported library
         obj = nx.Graph()
-        graph_interface = get_interface(obj)
-        self.assertIsNotNone(graph_interface)
-        self.assertIsInstance(graph_interface, Graph)
+        graph_interface = interface.get_interface(obj)
+        self.assertIsInstance(graph_interface, interface.BaseGraphInterface)
         # test with object not from supported library
         obj = str
-        graph_interface = get_interface(obj)
+        graph_interface = interface.get_interface(obj)
         self.assertIsNone(graph_interface)
         # test with object with no module property
         obj = 'str'
-        graph_interface = get_interface(obj)
+        graph_interface = interface.get_interface(obj)
         self.assertIsNone(graph_interface)
+
+
+class BaseGraphInterfaceTest:
+
+    class BaseGraphInterfaceTestCases(unittest.TestCase):
+
+        """ Unit tests for NetworkxGraph """
+
+        graph = None
+
+        edges = [
+            (0, 1), (0, 2), (0, 3),
+            (3, 6), (4, 5), (4, 6),
+            (5, 6)
+        ]
+
+        @property
+        def n_nodes(self):
+            return 1 + max(it.chain.from_iterable(self.edges))
+
+        def test_get_nodes(self):
+            nodes = self.graph.get_nodes()
+            expected_nodes = set(it.chain.from_iterable(self.edges))
+            self.assertSetEqual(set(nodes), expected_nodes)
+
+        def test_get_neighbors(self):
+            table = {
+                'node 0': {
+                    'node': 0,
+                    'nbrs': {1, 2, 3}
+                },
+                'node 1': {
+                    'node': 1,
+                    'nbrs': {0}
+                },
+                'node 2': {
+                    'node': 2,
+                    'nbrs': {0}
+                },
+                'node 3': {
+                    'node': 3,
+                    'nbrs': {0, 6}
+                },
+                'node 4': {
+                    'node': 4,
+                    'nbrs': {5, 6}
+                },
+                'node 5': {
+                    'node': 5,
+                    'nbrs': {4, 6}
+                },
+                'node 6': {
+                    'node': 6,
+                    'nbrs': {3, 4, 5}
+                },
+            }
+            for test_name, test in table.items():
+                nbrs = set(self.graph.get_neighbors(test['node']))
+                self.assertSetEqual(nbrs, test['nbrs'], test_name)
+
+        def test_get_neighborhood_features(self):
+            features = [
+                pd.Series(
+                    {
+                        0: 3, 1: 1, 2: 1, 3: 2,
+                        4: 2, 5: 2, 6: 3
+                    }
+                ).rename('degree'),
+                pd.Series(
+                    {
+                        0: 3, 1: 1, 2: 1, 3: 2,
+                        4: 3, 5: 3, 6: 4
+                    }
+                ).rename('internal_edges'),
+                pd.Series(
+                    {
+                        0: 1, 1: 2, 2: 2, 3: 4,
+                        4: 1, 5: 1, 6: 1
+                    }
+                ).rename('external_edges')
+            ]
+            expected_features = pd.concat(features, axis=1)
+            result_features = self.graph.get_neighborhood_features()
+            self.assertTrue(np.allclose(result_features, expected_features))
+
+
+class TestNetworkxInterface(BaseGraphInterfaceTest.BaseGraphInterfaceTestCases):
+
+    def setUp(self):
+        G = nx.Graph(self.edges)
+        self.graph = interface.NetworkxInterface(G)
+
+
+class TestIgraphInterface(BaseGraphInterfaceTest.BaseGraphInterfaceTestCases):
+
+    def setUp(self):
+        G = ig.Graph()
+        G.add_vertices(self.n_nodes)
+        G.add_edges(self.edges)
+        self.graph = interface.IgraphInterface(G)
