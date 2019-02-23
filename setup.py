4ec3de7739b6449f954450fa4d80e29cafac5327
dkaslovsky <dkaslovsky@gmail.com>
2019-02-22 19:22:21 -0700

    add setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+
+with open('README.md') as f:
+    long_description = f.read()
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+
+setup(
+    name='graphRole',
+    version='0.1.0',
+    author='Daniel Kaslovsky',
+    author_email='dkaslovsky@gmail.com',
+    license='MIT',
+    description='Automatic feature extraction and node role assignment for transfer learning on graphs',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=['graphrole'],
+    install_requires=requirements,
+    url='https://github.com/dkaslovsky/GraphRole',
+    keywords=['graph', 'feature extraction', 'transfer learning', 'network', 'graph analysis', 'network analysis'],
+    classifiers=[
+        'Environment :: Console',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ]
+)
