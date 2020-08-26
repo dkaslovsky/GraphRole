49b8b1166cbffe6e91558174286d9dbb56168761
dkaslovsky <dkaslovsky@gmail.com>
2020-08-25 20:21:53 -0600

    python3.8

--- setup.py
+++ setup.py
@@ -31,5 +31,6 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ]
 )
