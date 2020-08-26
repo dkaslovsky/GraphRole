6371a2ea4342370812507ea6e2ac23bb7ad15e12
dkaslovsky <dkaslovsky@gmail.com>
2020-08-25 20:19:12 -0600

    version bump to 1.0.1

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='graphrole',
-    version='1.0.0',
+    version='1.0.1',
     author='Daniel Kaslovsky',
     author_email='dkaslovsky@gmail.com',
     license='MIT',
